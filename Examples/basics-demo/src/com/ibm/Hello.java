23f3083104ec78343d86096f0e83c61abd0797a1
Kishor <kishor1124@gmail.com>
2025-04-04 12:33:07 +0530

    initial commit with activity

---
+++ Examples/basics-demo/src/com/ibm/Hello.java
@@ -0,0 +1,15 @@
+package com.ibm;
+/*
+ * main -> control + space -> generates main method
+ * sysout -> control + space -> generates S.o.p(...)
+ * if -> control + space -> shows list of if else combination
+ * try -> control + space -> show list of try catch combination
+ * highlight few lines -> control + / to comment or uncomment
+ */
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java Program! - 1");
+		System.out.println("Welcome to Java Program! - 2");
+		System.out.println("Welcome to Java Program! - 3");
+	}
+}
---
+++ Examples/basics-demo/src/com/ibm/TestFundamentals.java
@@ -0,0 +1,68 @@
+package com.ibm;
+
+public class TestFundamentals {
+
+	public static void main(String[] args) {
+		/* primitive datatypes and variables
+		 byte(1), short(2), int(4), long(8) - int values | 
+		 float(4) & double(8) - floating point values
+		 char(2) stores single character, boolean(unspecific) stores true or false
+		 byte range is -128 to -1, 0 to 127
+		 Primitive datatypes - int, float, double, char, boolean, byte, short, long
+		 Non-Primitive datatypes - Arrays, Userdefined classes and inbuilt classes
+		 Operators - +, -, *, /, %, <, >, !=, ==, ++, --
+		 */
+		int id = 2567; // it is an employee id
+		char gender = 'M'; // single quote
+		float salary = 45000.0f; // float needs f or F at the end else it is treated as double
+		boolean isMarried = false; // boolean takes true or false
+		String name = "Vijay"; // String is a complex type because it is of class type
+		
+		System.out.println("Id = "+id);
+		System.out.println("Name = "+name+", Salary = "+salary+", Gender = "+gender+", Is Married = "+isMarried);
+		int counter = 10;
+		int size = 15;
+		System.out.println("Counter = "+counter++); // post increment
+		System.out.println("Counter = "+counter); // 11
+		System.out.println("Size = "+(++size)); // pre increment
+		System.out.println("Size = "+size);
+		// conditional statements if, else
+		if(size > counter) {
+			System.out.println(size+" > "+counter);
+		} else {
+			System.out.println(size +" not > "+counter);
+		}  
+		// if else if else ladder - if(..){} else if(..) {} else if(..) {}... else
+		int marks = 80; // 0 to 100 -> display grades like A, B, C
+		if(marks >= 75) {
+			System.out.println("Grade A+");
+		} else if(marks < 75 && marks >= 70) {
+			System.out.println("Grade A");
+		} else if(marks < 70 && marks >= 60) {
+			System.out.println("Grade B");
+		} else {
+			System.out.println("Grade C");
+		}
+		/*
+		 *  try switch statement - it is similar to if else but it evaluates based on the options
+		 *  syntax: 
+		 *  switch(option) {
+		 *    case optionValue : some statement;
+		 *                         break;
+		 *    case optionValue : some statement;
+		 *                         break;
+		 *    default : some statement; // doesn't need break
+		 *                     
+		 *  }
+		 *  ex: 
+		 *  String option = "1";
+		 *  1 -> print have a coffee
+		 *  2 -> print have a tea
+		 *  3 -> print have a cold drink
+		 *  4 -> print have an hot water
+		 *  5 -> default value like no valid option thank you
+		 */
+		
+	}
+
+}
