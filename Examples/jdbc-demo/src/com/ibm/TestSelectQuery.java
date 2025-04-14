a475bd3ed56638782cbe022fc67c691ff392246f
Kishor <kishor1124@gmail.com>
2025-04-14 11:05:15 +0530

    jdbc read all

--- Examples/jdbc-demo/src/com/ibm/TestSelectQuery.java
+++ Examples/jdbc-demo/src/com/ibm/TestSelectQuery.java
@@ -1,12 +1,5 @@
 package com.ibm;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-import com.mysql.cj.jdbc.Driver;
 
 public class TestSelectQuery {
 	public static void main(String[] args) {
