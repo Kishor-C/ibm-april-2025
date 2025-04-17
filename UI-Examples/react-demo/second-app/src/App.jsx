61d7df7358db2584e69ddccda04d1e604ac02b97
Kishor <kishor1124@gmail.com>
2025-04-17 10:43:00 +0530

    react with axios

---
+++ UI-Examples/react-demo/second-app/src/App.jsx
@@ -0,0 +1,11 @@
+import { FetchAllUsers } from "./components/FakeUsers"
+
+
+function App() {
+  return (<div className="container-fluid">
+    <h1>Root Component</h1>
+    <FetchAllUsers />
+  </div>)
+}
+
+export default App
---
+++ UI-Examples/react-demo/second-app/src/components/FakeUsers.jsx
@@ -0,0 +1,30 @@
+import {useState} from 'react';
+import axios from 'axios';
+
+export function FetchAllUsers() {
+    let [users, setUsers] = useState([]);
+    let [id, setId] = useState("");
+    let [user, setUser] = useState({});
+    let URL = "https://jsonplaceholder.typicode.com/users";
+    // event handler to get a single user
+    let getSingleUser = (e) => {
+        axios.get(`${URL}/${id}`)
+        .then(res => setUser(res.data))
+        .catch(err => console.log(err));
+    }
+    return (<div>
+        <h2>Fetch users from json placeholder</h2><hr />
+        <input type = "number" placeholder='Enter id' 
+            onChange={e => setId(e.target.value)} className="w-25 form-control form-control-lg"/> <br />
+        <input type = "button" value = "Search"
+        onClick = {getSingleUser} className="btn btn-primary btn-lg"/>
+        <div className="alert alert-success">
+            <h3>
+                User Id = {user.id}, Username = {user.username}, Phone = {user.phone}, Email = {user.email}
+            </h3>
+            <h4>
+                Address = {user.address?.street}, {user.address?.city}, {user.address?.zipcode}
+            </h4>
+        </div>
+    </div>)
+}
